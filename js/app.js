c69179f40a1b9acb423c67812b581463fef9e859
Michal Margiel <Michal.Margiel@gmail.com>
2015-03-14 22:09:19 +0100

    working

---
+++ js/app.js
@@ -0,0 +1,26 @@
+angular.module('confitura', ['ngResource', 'ngSanitize', 'angular-loading-bar', 'ngAnimate'])
+    .config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
+        cfpLoadingBarProvider.includeSpinner = false;
+        moment.locale('pl');
+    }])
+    .factory('Twitter', function ($resource) {
+        return $resource('http://c4p.confitura.pl/api/twitter');
+    })
+    .factory('News', function ($resource) {
+        return $resource('http://c4p.confitura.pl/api/news/0/1');
+    })
+    .controller('TwitterCtrl', function ($scope, Twitter) {
+        Twitter.query(function (tweets) {
+            $scope.tweet = tweets[0];
+        });
+
+        $scope.getDate = function (timestamp) {
+            return moment(Date.parse(timestamp)).fromNow();
+        };
+    })
+    .controller('NewsCtrl', function ($scope, $sce, News) {
+        News.query(function (news) {
+            $scope.news = news[0];
+        })
+    })
+;
