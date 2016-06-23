38dcedac7b5b9df7bfcb306724b18fb0ba3f6a92
Michal Margiel <Michal.Margiel@gmail.com>
2016-06-23 16:10:56 +0200

    fix agenda style on iphonee

--- src/scripts/about/organizers.controller.js
+++ src/scripts/about/organizers.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* @ngInject */
-function OrganizersController(Organizers, PersonModal, $scope) {
+function OrganizersController(Organizers, PersonModal, $scope, $timeout) {
 	var vm = this;
 	vm.list = Organizers.query({type: 'main'});
 	vm.detailsOf = detailsOf;
@@ -10,5 +10,56 @@ function OrganizersController(Organizers, PersonModal, $scope) {
 		PersonModal.openFor([person]);
 	}
 	$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+	//$timeout(function () {
+	//	var mapCenter = new google.maps.LatLng(52.24041, 21.019376);
+	//	var mapOptions = {
+	//		scrollwheel: false,
+	//		zoom: 17,
+	//		center: mapCenter,
+	//		mapTypeId: google.maps.MapTypeId.ROADMAP,
+	//		styles: [
+	//			{
+	//				"featureType": "landscape",
+	//				"elementType": "geometry",
+	//				"stylers": [
+	//					{ "color": "#a2344d" }
+	//				]
+	//			},
+	//			{
+	//				"featureType": "poi",
+	//				"elementType": "geometry",
+	//				"stylers": [
+	//					{ "color": "#ac3f58" }
+	//				]
+	//			},
+	//			{
+	//				"featureType": "road",
+	//				"elementType": "geometry",
+	//				"stylers": [
+	//					{ "color": "#c56080" }
+	//				]
+	//			},
+	//			{
+	//				"elementType": "labels.text.fill",
+	//				"stylers": [
+	//					{ "color": "#ffffff" }
+	//				]
+	//			},
+	//			{
+	//				"elementType": "labels.text.stroke",
+	//				"stylers": [
+	//					{ "color": "#a2344d" }
+	//				]
+	//			}
+	//		]
+	//	};
+	//	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	//	//var mapMarker = new google.maps.Marker({
+	//	//	position: mapCenter,
+	//	//	map: map,
+	//	//	icon: "/images/marker.png"
+	//	//});
+	//});
 }
 module.exports = OrganizersController;
