819e6c087f83f676b5e649940729b8341dfa30a9
Michal Margiel <Michal.Margiel@gmail.com>
2017-06-21 20:21:04 +0200

    something

--- src/scripts/directives/page/PageDirective.js
+++ src/scripts/directives/page/PageDirective.js
@@ -1,13 +1,14 @@
 'use strict';
 /* @ngInject */
 function PageDirective(Pages) {
-	return {
-		scope: {name: '@'},
-		template: '<div ng-bind-html="content.text"></div>',
-		controller: /* @ngInject */function ($scope) {
-			$scope.content = Pages.get({name: $scope.name});
-		}
-	};
+    return {
+        scope: {name: '@'},
+        bindToController: {name: '@'},
+        template: '<div ng-bind-html="content.text"></div>',
+        controller: /* @ngInject */function ($scope) {
+            $scope.content = Pages.get({name: $scope.name});
+        }
+    };
 }
 
 module.exports = PageDirective;
